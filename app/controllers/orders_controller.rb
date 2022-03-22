86b63284a23a9b6e90cd9441ebb9a9c7c89b8e44
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-22 11:06:32 -0700

    adds product variable to orders controller & subtotal calulation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,28 @@
 class OrdersController < ApplicationController
 
   def create
+    pp current_user
+    product = Product.find_by(id: params[:product_id])
     order = Order.new(
-      
+      product_id: params[:product_id],
+      user_id: current_user.id,
+      quantity: params[:quantity],
+      subtotal: product.price * params[:quantity],
+      tax: 0.09,
+      total: (product.price * params[:quantity]) * 0.09
     )
-    render json: {message: "testing order create"}
+    order.save 
+    render json: {message: "You made a successful purchase!"}
   end
 
   def show
-    render json: {message: "testing order show"} 
+    order = Order.find_by(id: params[:id])
+    render json: order.as_json
   end
 
   def index
-    render json: {message: "testing order index"} 
+    order = Order.all
+    render json: order.as_json
   end
   
 end
